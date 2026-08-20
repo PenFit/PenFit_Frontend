c3d455d7b8b8d72869948e7408394218de4fbd0c
JJING_WONG <snow020216@gmail.com>
2026-08-20 21:34:18 +0900

    :tada: Chore: 프로젝트 초기 설정

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return(
+    <>
+      <div>app</div>
+      <Routes>
+        {/* <Route path="/" element={< />} /> */}
+      </Routes>
+    </>
+  )
+}
